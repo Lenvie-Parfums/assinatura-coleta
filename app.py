0622ed853562d2bc6b86a577f96de86a95f202a9
Rafael Miranda <raffa_newgate@hotmail.com>
2026-08-05 12:51:24 -0300

    retirada de drive formularios pra continuar rodando o app

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from utils.assinatura import AssinaturaCanvas
 from utils.formulario import FormularioRetirada
 from utils.gerador_pdf import GeradorPDF
-from utils.drive import upload_pdf_google_drive
 from utils.alterar_status import ConsultarNF
 import re
 import time
@@ -15,9 +14,6 @@ if "dados_formulario" not in st.session_state:
     form = FormularioRetirada()
     dados = form.exibir_formulario()
 
-    
-    
-
     if dados:
         st.session_state["dados_formulario"] = dados
         st.rerun()
@@ -29,49 +25,38 @@ else:
     assinatura = AssinaturaCanvas().capturar_assinatura()
 
     if assinatura is not None:
-            notas = dados["pedido"]
-            lista_notas = [p.strip() for p in re.split(r"[,\\-]", notas) if p.strip()]
+        notas = dados["pedido"]
+        lista_notas = [p.strip() for p in re.split(r"[,\-]", notas) if p.strip()]
 
-            if st.button("📄 Gerar PDF e Enviar para o Google Drive"):
-                pdf = GeradorPDF(dados, assinatura)
-                pdf_stream = pdf.gerar_pdf()
+        if st.button("📄 Gerar PDF e Atualizar Omie"):
+            pdf = GeradorPDF(dados, assinatura)
+            pdf_stream = pdf.gerar_pdf()
 
-                # 👉 Upload para o Google Drive
-                link_drive = upload_pdf_google_drive(
-                    pdf_bytes=pdf_stream, 
-                    nome_arquivo=f"comprovante_{dados['pedido']}.pdf"
-                )
+            st.download_button(
+                label="⬇ Baixar Comprovante",
+                data=pdf_stream,
+                file_name=f"comprovante_{dados['pedido']}.pdf",
+                mime="application/pdf"
+            )
 
-                st.success("✅ PDF gerado e enviado para o Google Drive com sucesso!")
-                st.markdown(f"[📂 Ver no Google Drive]({link_drive})", unsafe_allow_html=True)
+            for nota in lista_notas:
+                try:
+                    st.info(f"Atualizando Nota no Omie: {nota}")
+                    resultado = ConsultarNF(nota)
+                    time.sleep(1)
 
-    
-                st.download_button(
-                    label="⬇ Baixar Comprovante",
-                    data=pdf_stream,
-                    file_name=f"comprovante_{dados['pedido']}.pdf",
-                    mime="application/pdf"
-                    )
-                
-                for nota in lista_notas:
-                    try:
-                        st.info(f"Atualizando Nota no Omie: {nota}")
-                        resultado = ConsultarNF(nota)
-                        time.sleep(1)
+                    if resultado == True:
+                        st.success(f"✅ Nota {nota} processada com sucesso!")
+                    else:
+                        st.error(f"❌ Erro para atualizar a nota: {nota} - {resultado}")
+                except Exception as e:
+                    st.error(f"❌ Erro para atualizar a nota: {nota} - {e}")
 
-                        if resultado == True:
-                            st.success(f"✅ Nota {nota} processada com sucesso!")
-                        else:
-                            st.error(f"❌ Erro para atualizar a nota: {nota} - {resultado}")
-                    except Exception as e:
-                        st.error(f"❌ Erro para atualizar a nota: {nota} - {e}")
-                st.success("✅ Todas as notas foram processadas!")
+            st.success("✅ Todas as notas foram processadas!")
+
     else:
         st.warning("✍️ Por favor, assine no campo acima.")
 
-        
     if st.button("🔄 Nova Retirada"):
         st.session_state.clear()
         st.rerun()
-    
-    
